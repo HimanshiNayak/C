5bc9baf451614bca5868c8c7ae63bfee24c61b50
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-01-31 15:43:49 +0530

    Add files via upload

---
+++ practice3.c
@@ -0,0 +1,43 @@
+// // 1. rectangle 
+
+// #include <stdio.h>
+// int main(){
+//     printf("*****\n");
+//     printf("*****\n");
+//     printf("*****\n");
+//     printf("*****\n");
+//     printf("*****\n*****\n*****\n*****");
+//     return 0;
+// }
+
+
+// // 2.pattern
+// #include<stdio.h>
+// int main(){
+//     printf("1*********\n12********\n123*******\n1234******\n12345*****");
+//     return 0;
+// }
+
+
+// // 3.x character
+// #include <stdio.h>
+// int main (){
+//             //  printf("*     *\n *   *\n  **\n  *\n  **\n *   *\n*      *");
+//             printf("*    *\n");
+//             printf(" *   *\n");
+//             printf("  * *\n");
+//             printf("   *\n");
+//             printf("  * *\n");
+//             printf(" *    *\n");
+//             printf("*      *\n");
+//             return 0;
+// }
+// birth year and age calculation
+#include <stdio.h>
+// #include <stdlib.h>
+int main()
+{
+    printf("i was born in : %d \n" , 2022-18);
+    // printf("in the year %d , i will become %d years old" ,2023 ,2023-2004);
+    return 0;
+}
---
+++ practice4.c
@@ -0,0 +1,80 @@
+
+// #include <stdio.h>
+// int main (){
+//     // define its type , then name and its value
+//     int num ;
+//     num = 5;
+//     printf("the value of num is : %d\n",num);
+//     // taking input :scanf("type",&where to put the value)
+//     int grade1 ;
+//     scanf("%d" ,&grade1);
+//     printf("the grades of first student is: %d\n" , grade1);
+//     int grade2 ;
+//     scanf("%d",&grade2);
+//     printf("the grades of second student are: %d\n",grade2);
+//     int average = (grade1+grade2)/2;
+//     printf("the average is:%d ", average);
+//     // here it should be same as the one we defined it as :if %d then use d again ..
+//     return 0;
+// }
+
+
+// take input and tell the birth year :
+
+
+// #include <stdio.h>
+// int main(){
+//     int currentyear;
+//     int age;
+//     int birthyear;
+//     scanf(" %d \n",&currentyear);
+//     scanf(" %d",&age);
+//     birthyear = currentyear-age;
+//     printf("current year: %d\n",currentyear);
+//     printf("your current age is : %d\n",age);
+//     printf("you were born in the year: %d ",currentyear-age );
+//     return 0;
+// }
+
+
+// rectangle's area
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// this is compulsory for the output one
+
+// int main(){
+//     double height , width;
+//     // The double in C is a data type that is used to store high-precision floating-point data or numbers (up to 15 to 17 digits). It is used to store large values of decimal numbers.
+//     // to specify double , we use lf 
+//     printf("enter height: ");
+//     scanf("%lf" , & height);
+//     printf("enter  width: ");
+//     scanf("%lf",&width);
+//     printf("the area of the rectangle will be %.1lf\n" , height*width);
+//     // %.1lf is to get only one place after point
+//     return 0;
+
+// }
+
+// rectangles perimeter:
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main(){
+//     double height,width;
+//     double perimeter;
+//     printf("Enter the height of the rectangle: ");
+//     scanf("%lf" , &height);
+//     printf("Enter the width of the rectangle: ");
+//     scanf("%lf" , &width);
+//     perimeter = 2*(height+width);
+//     printf("the perimeter of the given rectangle is = %.2lf",perimeter);
+//     return 0;
+
+// }
+
+// area of circle:
+
