d3f15c1cb23821a61071b82b01b8a46883f1252e
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-02-12 23:27:44 +0530

    Add files via upload

---
+++ casting.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+    int num1 = 5 ,  num2 = 2;
+    int result;
+    result = num1/num2;
+    printf("the result = %d " , result);
+
+    float num3 = 5, num4 = 2;
+    float results;
+    results = num1/num2;
+    printf("the result = %f " , results);
+
+    int num1 = 5 ,  num2 = 2;
+    float results;
+    results = num1/num2;
+    printf("the result = %f " , results);
+
+    return 0 ;
+}
+// int wih float is float
+
+// when we have two int but if we want the result to be float;
+
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+    int num1 = 5 ,  num2 = 2;
+    double result;
+    result = num1/(double)num2;
+    printf("the result = %lf" , result);
+    return 0 ;
+}
