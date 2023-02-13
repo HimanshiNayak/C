f910d58448d7cbd79704d7b34c7cd1b8e8e89050
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-02-13 22:59:27 +0530

    Add files via upload

---
+++ bits.c
@@ -0,0 +1,26 @@
+//  a bit can either be 0 or 1
+// bit - basic unit to represent a value
+// byte :- a sequence of 8 bits
+// decimal numbers :- consists of 10 digits from 0-9
+// base10:- we can get new by just multiplying with powers of 10(decimal no.)
+// weight = base ^position
+
+
+// decimal values             binary values
+// 0                                 0
+// 1                                 1
+// 2                                 10
+// 3                                 11
+// 4                                 100
+// 5                                 101
+// 6                                 110
+// 7                                 111
+// 8                                 1000
+
+
+// binary no. has a base 2:- 
+// binary to decimal :-
+// 1101 = 1*2^3 + 1*2^2 + 0*2^1 + 1*2^0 = 8+4+1=13
+// weight = base^position
+
+
