b703ea8bcd59e515b34adad25dcacdee3c8df19b
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-02-20 23:55:27 +0530

    Add files via upload

---
+++ basicspractice.c.cpp
@@ -0,0 +1,53 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int i =3;
+//	int l = i/-2;
+//	int k = i%-2;
+//	printf("%d %d\n",l,k);
+//	return 0;
+////}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10 ,b =5, c=5;
+//	int d;
+//	d = b +c ==a;
+//	printf("%d",d);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("*        *\n");
+//	printf("  *    *\n");
+//	printf("    *\n");
+//	printf("    *\n");
+//	printf("    *\n");
+//	printf("    *\n");
+//	
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10;
+////	scanf("%d",a);
+//	printf("Ayush is very good %d:", a);
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	char str[100];
+//	str a = "Hello world"
+//	printf("%s\n,%s",Hello World!",Welcom to C programming);
+	scanf("%s",str);
+	printf("Welcome to C programming");
+	return 0;
+}
+
+
