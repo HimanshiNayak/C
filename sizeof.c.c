2e1e0fa35d784ac820e9f349f4baec0cd6611255
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-02-20 23:52:51 +0530

    Add files via upload

---
+++ sizeof.c.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void main()
+{
+    // int a =5;
+    // long a =6; # it wont be wrong as its not a strict language
+    // we can't write short amd long ogether
+    // long b = 6;
+    // printf()...................
+ 
+    // printf("Hello World \n");
+    // // printf("5");
+    // // printf("%d",a);
+    // // printf("%c",a);
+    // // printf("%d",sizeof(0.7));
+    // // printf("%d",sizeof(0.7f));
+    // printf(
+
+    // )
+
+    // int a =15 ,b=10;
+    // printf("\n addition = %d",a+b);
+    // no need to write the long method , just use like this
+
+
+    char ch = 128;
+    printf("%d",ch); 
+    // gives -128 as moves clockwise and we have till 127 only , int's range is small ...thus we use long to increase the range
+
+    // for 129 , it will give , -127;
+}
