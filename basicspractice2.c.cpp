ebff724440fcf6681d4114f5c0988f615c2be99b
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-02-20 23:56:35 +0530

    Add files via upload

---
+++ basicspractice2.c.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+    int a , b ,c ,d;
+    b= 10;
+    a =5;
+//   c = ++a;
+//   d = a++;
+//    c = ++a;
+//    d = (a++)++; errror as will give 6 and 6 is constant
+//    printf("\n the value of a is :%d",a);
+//  c =a<<2;
+//  d= b<<2;
+
+ c = a<<2<<2;sz
+
+    printf("\n the value of c is :%d",c);
+   printf("\n the value of d is :%d",d);
+    return 0;
+}
