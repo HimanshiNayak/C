bbdee56b1ca98d27c8071858c8fed413af73623c
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-03-09 23:01:58 +0530

    Add files via upload

---
+++ while loop.cpp
@@ -0,0 +1,29 @@
+// takes the value , checks the condition and then executes
+
+//1. print * 10 times
+
+//#include <stdio.h>
+//int main(){
+//	int i= 1 ;
+//	while (i<=10){
+//		printf("*");
+//		i = i+1;
+//	}
+//	return 0;
+//}
+
+//2. take the number of * to be printed from the user and print
+
+# include <stdio.h>
+int main(){
+	int a;
+	int i; //to start from 1 , define i=1
+	printf("Enter the number of * to be printed:\n");
+	scanf("%d",&a);
+	while (i<=a)
+	{
+		printf("*");
+		i++;
+	}
+	return 0;
+}
