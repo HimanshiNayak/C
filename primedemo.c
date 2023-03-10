3db5eb15b04943df4c66a8e1892cd971ba28e069
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-03-10 22:24:58 +0530

    Add files via upload

---
+++ primedemo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	int i,n; 
+	int flag = 0; 	
+	printf("\nEnter number of line = ");
+	scanf("%d",&n);
+	for(i = 2; i < n; i++)//line control or Row control
+	{
+		if(n%i==0)
+		{
+			flag = 1; 
+			break;
+		}		
+	}
+	if(flag == 1)
+	{
+		printf("Not Prime");
+	}
+	else
+	{
+		printf(" Prime");
+	}
+	 
+}
+
