abffd3f5a0853c5443dddde3804a879069ef80f9
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-03-16 20:14:05 +0530

    Add files via upload

---
+++ conditionals.cpp
@@ -0,0 +1,32 @@
+// char can have only one thing , space is also not allowed
+
+//if -else
+//if(condition){
+      //expression;
+//  }
+//  else {
+//  	expression;
+//  }
+
+// else is not independent , not necessary also
+
+//adult or not
+
+#include<stdio.h>
+int main(){
+ int age;
+ printf("Enter your age:\n");
+ scanf("%d",&age);
+
+ if(age>=18){
+ 	printf("You are an adult.\n");
+ 	printf("You can drive.\n");
+ }
+ else{
+ 	printf("You are not an adult.\n");
+ }
+ printf("Thank you"); // will always be printed
+ return 0;
+}
+
+
