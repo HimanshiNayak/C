0ad53e264949e06ffb52c079524db9ab0f7bfce6
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-04-06 23:30:39 +0530

    Add files via upload

---
+++ func.2.cpp
@@ -0,0 +1,16 @@
+// to print the table of n:
+//#include<stdio.h>
+//void table(int a);
+//int main(){
+//	int a ;
+//	printf("Enter a  number: \n");
+//	scanf("%d",&a);
+//    Table(a); // argument / actual parameter
+//	return 0;
+//}
+//void table(int a){ //parameter / formal parameter
+//	for (int i =1 ; i<=10;i++)){
+//		printf("%d\n",i*a);
+//	}
+//}
+
