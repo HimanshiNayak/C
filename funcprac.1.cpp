793bc725db180a6fe2286979c91d96535319923c
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-04-18 23:29:02 +0530

    Update funcprac.1.cpp

--- funcprac.1.cpp
+++ funcprac.1.cpp
@@ -2,18 +2,18 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-//int max(int a , int b , int c){
-//	if (a>b){
-//		if (a>c)
-//		    return a;
-//		else 
-//		    return c;	
-//	}
-//	else if (b>c)
-//	    return b;
-//	else 
-//	    return c;
-//}
+int max(int a , int b , int c){
+	if (a>b){
+		if (a>c)
+		    return a;
+		else 
+		    return c;	
+	}
+	else if (b>c)
+	    return b;
+	else 
+	    return c;
+}
 //
 int main(){
 	int a,b,c;
