e46cdbfc122c5e83da450fcf0bb9bc65220959a3
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-04-19 23:50:28 +0530

    Update funcprac.2.cpp

--- funcprac.2.cpp
+++ funcprac.2.cpp
@@ -16,3 +16,24 @@ int main(){
 	printf("the area of the rectangle is: %f",area(l,b));
 	return 0;
 }
+
+#include <stdio.h>
+void maxdigit(int num ){
+	if (num >99 || num<10 )
+	    printf("The number is not a 2-digit number!!");
+	else
+	    {
+	      if (num%10 > num/10)	
+	          printf("Maximum digit in %d is %d :",num,num%10);
+	      else 
+	          printf("Maximum digit in %d is %d :",num,num/10);
+		}
+}
+
+int main(){
+	int num ;
+	printf("Enter a number: ");
+	scanf("%d",&num);
+	maxdigit(num);
+	return 0;
+}
