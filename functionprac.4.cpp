c2d07b0707abf17654d1e89482c00c93b0a240f0
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-04-19 23:58:21 +0530

    Add files via upload

---
+++ functionprac.4.cpp
@@ -0,0 +1,53 @@
+
+//#include <stdio.h>
+//void evenodd(int a){
+//	if (a%2 == 0){
+//		printf("%d is even.",a);
+//	}
+//	else 
+//	    printf("%d is odd.",a);
+//}
+//int main(){
+//	int a ;
+//	printf("Enter a number: ");
+//	scanf("%d",&a);
+//	evenodd(a);
+//	return 0;
+//}
+
+// isodd(gives 1 when odd otherwise 0) 
+//#include <stdio.h>
+//int isodd(int a){
+//	if (a%2 != 0){
+//		return 1;
+//	}
+//	else 
+//	    return 0;
+//}
+//int main(){
+//	int a ;
+//	int result;
+//	printf("Enter a number: ");
+//	scanf("%d",&a);
+//	result = isodd(a);
+//	printf("%d",result);
+//	return 0;
+//}
+// iseven(gives 1 when even otherwise 0) 
+//#include <stdio.h>
+//int iseven(int a){
+//	if (a%2 == 0){
+//		return 1;
+//	}
+//	else 
+//	    return 0;
+//}
+//int main(){
+//	int a ;
+//	int result;
+//	printf("Enter a number: ");
+//	scanf("%d",&a);
+//	result = iseven(a);
+//	printf("%d",result);
+//	return 0;
+//}
